978c2abc981b4c386917ad9399f83ae744ce1410
sbelbin <sbelbin@neuroservo.com>
2024-05-14 12:27:42 -0400

    Converting to a simple login authentication for Horizon

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
-import { Authenticator } from '@aws-amplify/ui-react'
+
+import {
+  Authenticator,
+  Button,
+  Heading,
+  Image,
+  Text,
+  View,
+  useAuthenticator,
+  useTheme
+} from '@aws-amplify/ui-react';
+
 import '@aws-amplify/ui-react/styles.css'
 
 const client = generateClient<Schema>();
@@ -23,8 +34,239 @@ function App() {
     client.models.Todo.delete({ id });
   }
 
+  const components = {
+    Header() {
+      const { tokens } = useTheme();
+
+      return (
+        <View textAlign="center" padding={tokens.space.large}>
+          <Image
+            alt="NeuroServo logo"
+            height="40%"
+            width="40%"
+            src="/public/img/neuroservo.png"
+          />
+        </View>
+      );
+    },
+
+    Footer() {
+      const { tokens } = useTheme();
+
+      return (
+        <View textAlign="center" padding={tokens.space.large}>
+          <Text color={tokens.colors.white}>
+            &copy; All Rights Reserved
+          </Text>
+        </View>
+      );
+    },
+
+    SignIn: {
+      Header() {
+        const { tokens } = useTheme();
+
+        return (
+          <Heading
+            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+            level={3}
+          >
+            Sign in to your account
+          </Heading>
+        );
+      },
+      Footer() {
+        const { toForgotPassword } = useAuthenticator();
+
+        return (
+          <View textAlign="center">
+            <Button
+              fontWeight="normal"
+              onClick={toForgotPassword}
+              size="small"
+              variation="link"
+            >
+              Reset Password
+            </Button>
+          </View>
+        );
+      },
+    },
+
+    SignUp: {
+      Header() {
+        const { tokens } = useTheme();
+
+        return (
+          <Heading
+            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+            level={3}
+          >
+            Create a new account
+          </Heading>
+        );
+      },
+      Footer() {
+        const { toSignIn } = useAuthenticator();
+
+        return (
+          <View textAlign="center">
+            <Button
+              fontWeight="normal"
+              onClick={toSignIn}
+              size="small"
+              variation="link"
+            >
+              Back to Sign In
+            </Button>
+          </View>
+        );
+      },
+    },
+    ConfirmSignUp: {
+      Header() {
+        const { tokens } = useTheme();
+        return (
+          <Heading
+            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+            level={3}
+          >
+            Enter Information:
+          </Heading>
+        );
+      },
+      Footer() {
+        return <Text>Footer Information</Text>;
+      },
+    },
+    SetupTotp: {
+      Header() {
+        const { tokens } = useTheme();
+        return (
+          <Heading
+            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+            level={3}
+          >
+            Enter Information:
+          </Heading>
+        );
+      },
+      Footer() {
+        return <Text>Footer Information</Text>;
+      },
+    },
+    ConfirmSignIn: {
+      Header() {
+        const { tokens } = useTheme();
+        return (
+          <Heading
+            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+            level={3}
+          >
+            Enter Information:
+          </Heading>
+        );
+      },
+      Footer() {
+        return <Text>Footer Information</Text>;
+      },
+    },
+    ForgotPassword: {
+      Header() {
+        const { tokens } = useTheme();
+        return (
+          <Heading
+            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+            level={3}
+          >
+            Enter Information:
+          </Heading>
+        );
+      },
+      Footer() {
+        return <Text>Footer Information</Text>;
+      },
+    },
+    ConfirmResetPassword: {
+      Header() {
+        const { tokens } = useTheme();
+        return (
+          <Heading
+            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+            level={3}
+          >
+            Enter Information:
+          </Heading>
+        );
+      },
+      Footer() {
+        return <Text>Footer Information</Text>;
+      },
+    },
+  };
+
+  const formFields = {
+    signIn: {
+      username: {
+        placeholder: 'Enter your email',
+      },
+    },
+    signUp: {
+      password: {
+        label: 'Password:',
+        placeholder: 'Enter your Password:',
+        isRequired: false,
+        order: 2,
+      },
+      confirm_password: {
+        label: 'Confirm Password:',
+        order: 1,
+      },
+    },
+    forceNewPassword: {
+      password: {
+        placeholder: 'Enter your Password:',
+      },
+    },
+    forgotPassword: {
+      username: {
+        placeholder: 'Enter your email:',
+      },
+    },
+    confirmResetPassword: {
+      confirmation_code: {
+        placeholder: 'Enter your Confirmation Code:',
+        label: 'New Label',
+        isRequired: false,
+      },
+      confirm_password: {
+        placeholder: 'Enter your Password Please:',
+      },
+    },
+    setupTotp: {
+      QR: {
+        totpIssuer: 'test issuer',
+        totpUsername: 'amplify_qr_test_user',
+      },
+      confirmation_code: {
+        label: 'New Label',
+        placeholder: 'Enter your Confirmation Code:',
+        isRequired: false,
+      },
+    },
+    confirmSignIn: {
+      confirmation_code: {
+        label: 'New Label',
+        placeholder: 'Enter your Confirmation Code:',
+        isRequired: false,
+      },
+    },
+  };
+
   return (
-    <Authenticator>
+    <Authenticator components={components}
+                   formFields={formFields}
+                   socialProviders={['google']}>
       {({ signOut, user }) => (
         <main>
           <h1>{user?.signInDetails?.loginId}'s todos</h1>
